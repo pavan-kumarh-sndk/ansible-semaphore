4c64f1b5bd9b12e41522c6d493db5112d628aeb7
pavan-kumarh-sndk <157478910+pavan-kumarh-sndk@users.noreply.github.com>
2025-07-08 15:37:05 +0530

    Create nvme_list.py

---
+++ nvme_list.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/env python3
+import subprocess
+
+cmd = 'nvme list'
+
+try:
+    p = subprocess.Popen('echo Auto_BGA_1234|sudo -S '+cmd, shell=True, stdout=>
+                                         stderr=subprocess.STDOUT)
+    result = [x.decode("utf-8") for x in p.stdout.readlines()]
+    if '/dev/nvme0' in (result[2].split(' ')[0]):
+        print('Pass')
+    else:
+        print('Fail')
+except:
+    print('Fail')
